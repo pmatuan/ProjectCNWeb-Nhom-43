cbe89a62f6b9e3ec6b6923836c5bd075be160431
pmatuan <tuan.pma205227@sis.hust.edu.vn>
2023-04-06 21:56:48 +0700

    Commit message

---
+++ src/models/quizes.js
@@ -0,0 +1,81 @@
+const mongoose = require('mongoose');
+
+const optionSchema = new mongoose.Schema({
+  option: {
+    type: String,
+    required: true,
+  },
+});
+
+const questionSchema = new mongoose.Schema(
+  {
+    question: {
+      type: String,
+      required: true,
+    },
+    answers: [optionSchema],
+
+    answer: {
+      type: Number,
+      required: true,
+    },
+
+    isEnabled: {
+      type: Boolean,
+      default: true,
+    },
+
+    explanation: {
+      type: String,
+      default: '',
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+const quizSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    instructions: {
+      type: String,
+      required: true,
+    },
+
+    isEnabled: {
+      type: Boolean,
+      default: true,
+    },
+
+    questions: [questionSchema],
+
+    duration: {
+      hours: {
+        type: Number,
+        default: 0,
+      },
+
+      minutes: {
+        type: Number,
+        default: 0,
+      },
+
+      seconds: {
+        type: Number,
+        default: 0,
+      },
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+var Quizes = mongoose.model('Quiz', quizSchema);
+
+module.exports = Quizes;
