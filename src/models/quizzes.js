7abe3cdbc8bb0dcb961b1d3b88385fc91ffab443
Anh Tuan <anhtuan9702@gmail.com>
2023-05-12 22:13:26 +0700

    Update quizzes models, routes, controllers

---
+++ src/models/quizzes.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+const questionSchema = new mongoose.Schema(
+  {
+    question: {
+      type: String,
+      required: true,
+    },
+    answers: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+    key: {
+      type: Number,
+      required: true,
+    },
+    explanation: {
+      type: String,
+      default: '',
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  },
+);
+
+const quizSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    questions: [questionSchema],
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+    collection: 'quizzes',
+  },
+);
+
+module.exports = mongoose.model('Quiz', quizSchema);
